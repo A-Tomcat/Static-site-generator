5d697d4b3bc27aa991e1cc93ff1d93f5426f2446
A-Tomcat <tom@ollfisch.de>
2025-12-27 19:17:11 +0100

    C: Starting on textnode test.sh

--- src/textnode.py
+++ src/textnode.py
@@ -8,15 +8,15 @@ class TextType(Enum):
     LINK = "[anchor text](url)"
     IMAGE = "![alt text](url)"
 
-class TextNode(Enum):
+class TextNode():
     def __init__(self, text, TextType, url = None):
         self.text = text
         self.text_type = TextType
         self.url = url
     
     def __eq__(self, Node):
-        if self.text == Node.text and self.text_type == Node.TextType and self.url == Node.url:
-            return True
+        return super().__eq__(Node)
         
     def __repr__(self):
-        return f"{self}({self.text}, {self.text_type}, {self.url})"
+        return f"TextNode({self.text}, {self.text_type}, {self.url})"
+    
