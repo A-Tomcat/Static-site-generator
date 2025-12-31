dcbc432343b9daf828e2ce93232d9ed138727353
A-Tomcat <tom@ollfisch.de>
2025-12-31 11:41:13 +0100

    J: MD conversion done. just missing tests.

--- src/main.py
+++ src/main.py
@@ -4,6 +4,10 @@ print("hello world")
 def main():
     node = TextNode("Link to main Website", TextType.LINK, "https://boot.dev")
     print(node)
+    node2 = TextNode("Test text with a **bold word** in the middle.", TextType.TEXT)
+    print(node2)
+    text_part = node2.text.split("**")
+    print(text_part)
 
 
 
--- src/textnode.py
+++ src/textnode.py
@@ -51,9 +51,20 @@ def text_node_to_html_node(text_node):
 def split_nodes_delimiter(old_nodes, delimiter, text_type):
     new_nodes = []
     for node in old_nodes:
-        if node is not TextType.TEXT:
+        if node.text_type != TextType.TEXT:
             new_nodes.append(node)
             continue
-        match delimiter:
-            case "**":
-                pass
+
+        text_part = node.text.split(delimiter)
+
+        if len(text_part) % 2 == 0:
+            raise Exception(f"Invalid Markdown Syntax!")
+        for i in range(0, len(text_part)):
+            if text_part[i] == "":
+                continue
+            if i % 2 == 0:
+                new_nodes.append(TextNode(text_part[i], TextType.TEXT))
+            else:
+                new_nodes.append(TextNode(text_part[i], text_type))
+    return new_nodes
+
